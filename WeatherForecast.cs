1c9c3d7cc39fd9e9b9ca95851ff39cbb9e068c76
ClenildonFerreira-Avine <desenvolvimento@avinealimentos.com.br>
2025-08-26 19:17:27 -0300

    first commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace monitoring_hard_api
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
